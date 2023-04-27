441dd041e5c5072ee8d2ad5287b599855cdaf0dd
juhee kang <mnmn092631@gmail.com>
2023-04-27 09:33:21 +0900

    MyClockTime setInterval 수정

--- src/02/MyClockTime.js
+++ src/02/MyClockTime.js
@@ -5,31 +5,10 @@ const MyClockTime = () => {
   const [timer, setTimer] = useState(
     new Date().toLocaleTimeString().replaceAll(":", " : ")
   );
-
-  const currentTimer = () => {
-    const date = new Date();
-    let ma = "오전";
-    let h = date.getHours();
-    if (h === 24) {
-      h = 0;
-    } else if (h >= 12) {
-      ma = "오후";
-      if (h > 12) h -= 12;
-    }
-    const m = date.getMinutes();
-    const s = date.getSeconds();
-    setTimer(
-      `${ma} ${h.toString().padStart(2, 0)} : 
-      ${m.toString().padStart(2, 0)} : 
-      ${s.toString().padStart(2, 0)}`
-    );
-  };
-
-  const startTimer = () => {
-    setInterval(currentTimer, 1000);
-  };
-
-  startTimer();
+  setInterval(
+    () => setTimer(new Date().toLocaleTimeString().replaceAll(":", " : ")),
+    1000
+  );
 
   return (
     <footer>
