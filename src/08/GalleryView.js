32dee55917dc272682c857a17a5b79738c57f6e9
juhee kang <mnmn092631@gmail.com>
2023-05-11 13:44:19 +0900

    GalleryWrapper width 수정

--- src/08/GalleryView.js
+++ src/08/GalleryView.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 import GalleryItem from "./GalleryItem";
 
 const GalleryWrapper = styled.div`
-  width: 100%;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
